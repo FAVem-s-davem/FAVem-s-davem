7c576b55075e150c6e16d1b96251438b12f2c5cb
laadim <cakora.ladislav@gmail.com>
2026-03-05 20:24:33 +0100

    project setup, basic movement
    setup c++ godot project, installed godot lib, formater, linter, lsp
    settings, attached scripts to node

---
+++ src/src/register_types.cpp
@@ -0,0 +1,35 @@
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/godot.hpp>
+
+#include "player.hpp"
+
+using namespace godot;
+
+void initialize_player_module(ModuleInitializationLevel level) {
+    if (level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    ClassDB::register_class<Player>();
+}
+
+void uninitialize_player_module(ModuleInitializationLevel level) {
+    if (level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+
+GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress addr,
+                                                GDExtensionClassLibraryPtr library,
+                                                GDExtensionInitialization *init) {
+    GDExtensionBinding::InitObject init_obj(addr, library, init);
+
+    init_obj.register_initializer(initialize_player_module);
+    init_obj.register_terminator(uninitialize_player_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+    return init_obj.init();
+}
+}
