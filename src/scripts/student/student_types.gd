c429e8d446eb41b07be36777fdd8a33bbc5a115a
Vokymir <voky.consultant@gmail.com>
2026-03-24 21:08:28 +0100

    WIP: how to represent student types?

--- src/scripts/student/student_types.gd
+++ src/scripts/student/student_types.gd
@@ -3,14 +3,24 @@ class_name StudentTypes
 enum Type {
 	MATH,
 	CS,
+	GEO,
+	INF_GEO,
+	MECH
 }
 
-const STUDENT_TYPE_NAMES := [
-	"math",
-	"inf",
-]
+
+const STUDENT_TYPE_INFO := {
+	Type.MATH: {"path": "math", "variations_count": 4},
+	Type.CS: {"path": "inf", "variations_count": 6},
+	Type.GEO: {"path": "geo", "variations_count": 4},
+	Type.INF_GEO: {"path": "infgeo", "variations_count": 5},
+	Type.MECH: {"path": "mech", "variations_count": 4},
+}
 
 static func student_type_to_string(type: int) -> String:
-	if type >= 0 and type < STUDENT_TYPE_NAMES.size():
-		return STUDENT_TYPE_NAMES[type]
+	if type in student_type_info.keys():
+		var info = student_type_info[type]           # get the dictionary/object
+		var index = randi_range(1, info["variations_count"])  # random variation
+		var index_str = str(index).pad_zeros(2)      # always two digits
+		return info["path"] + "_" + index_str        # construct filename
 	return "unknown"
